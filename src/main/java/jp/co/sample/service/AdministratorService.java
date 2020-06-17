fcc792052f2e4803f2507b4c17de7c327a8e6a38
HyogaIto <hyoga.ito@rakus.co.jp>
2020-06-17 15:25:45 +0900

    serviceにlogin処理を追記

--- src/main/java/jp/co/sample/service/AdministratorService.java
+++ src/main/java/jp/co/sample/service/AdministratorService.java
@@ -12,10 +12,9 @@ import jp.co.sample.repository.AdministratorRepository;
 public class AdministratorService {
 	@Autowired
 	private AdministratorRepository administratorRepository;
-	
-	
+
 	/**
-	 *  「管理者情報を挿入する」ために、リポジトリのinsert()を呼ぶ.
+	 * 「管理者情報を挿入する」ために、リポジトリのinsert()を呼ぶ.
 	 * 
 	 * @param administrator
 	 */
@@ -23,4 +22,15 @@ public class AdministratorService {
 		administratorRepository.insert(administrator);
 	}
 
+	/**
+	 *ログイン処理をする.
+	 * 
+	 * @param mailAddress　メールアドレス
+	 * @param password　パスワード
+	 * @return　管理者情報
+	 */
+	public Administrator login(String mailAddress, String password) {
+		Administrator administrator = administratorRepository.findByMailAddressAndPassword(mailAddress, password);
+		return administrator;
+	}
 }
