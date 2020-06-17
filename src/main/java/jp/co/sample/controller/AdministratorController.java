471092797851fc05c9b824afd47d666fe83cea55
HyogaIto <hyoga.ito@rakus.co.jp>
2020-06-17 14:20:39 +0900

    管理者情報登録画面の作成
    管理者情報挿入のため、insert()メソッドを追加
    
    管理者情報登録のため、Controllerにinsertメソッドを追加
    
    書き間違いを修正

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package jp.co.sample.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
 import jp.co.sample.service.AdministratorService;
 
@@ -42,5 +44,21 @@ public class AdministratorController {
 	public String toInsert() {
 		return "administrator/insert";
 	}
+	
+	/**
+	 * 管理者情報を登録する.
+	 * 
+	 * @param insertAdministratorForm 管理者情報
+	 * @return　ログイン画面
+	 */
+	@RequestMapping("/insert")
+	public String insert(InsertAdministratorForm insertAdministratorForm) {
+		Administrator administrator=new Administrator();
+		BeanUtils.copyProperties(insertAdministratorForm, administrator);
+		administratorService.insert(administrator);
+		return "redirect:/";
+	}
+	
+	
 
 }
